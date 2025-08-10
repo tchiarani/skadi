32562fe37d144ef2ccf1738fb39310c20ba51535
tchiarani <44852062+tchiarani@users.noreply.github.com>
2025-08-10 14:06:32 +0200

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,89 @@
+const minecraftVersion = "1.21.6";
+
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleBtnTooltip = document.getElementById('tooltip-theme-text');
+
+window.onload = function(){
+    document.title = "SKADI - " + minecraftVersion;
+    const versionSpans = document.getElementsByClassName('minecraft-version');
+    for (let i = 0; i < versionSpans.length; i++) {
+        versionSpans[i].innerHTML = minecraftVersion;
+    }
+};
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+themeToggleBtn.addEventListener('click', function() {
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            themeToggleBtnTooltip.innerHTML = 'Thème clair';
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            themeToggleBtnTooltip.innerHTML = 'Thème sombre';
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            themeToggleBtnTooltip.innerHTML = 'Thème clair';
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            themeToggleBtnTooltip.innerHTML = 'Thème sombre';
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
+
+window.addEventListener('load', function () {
+    const clipboard = FlowbiteInstances.getInstance('CopyClipboard', 'npm-install-copy-button');
+    const tooltip = FlowbiteInstances.getInstance('Tooltip', 'tooltip-copy-npm-install-copy-button');
+
+    const $defaultIcon = document.getElementById('default-icon');
+    const $successIcon = document.getElementById('success-icon');
+
+    const $defaultTooltipMessage = document.getElementById('default-tooltip-message');
+    const $successTooltipMessage = document.getElementById('success-tooltip-message');
+
+    clipboard.updateOnCopyCallback((clipboard) => {
+        showSuccess();
+
+        // reset to default state
+        setTimeout(() => {
+            resetToDefault();
+        }, 2000);
+    })
+
+    const showSuccess = () => {
+        $defaultIcon.classList.add('hidden');
+        $successIcon.classList.remove('hidden');
+        $defaultTooltipMessage.classList.add('hidden');
+        $successTooltipMessage.classList.remove('hidden');    
+        tooltip.show();
+    }
+
+    const resetToDefault = () => {
+        $defaultIcon.classList.remove('hidden');
+        $successIcon.classList.add('hidden');
+        $defaultTooltipMessage.classList.remove('hidden');
+        $successTooltipMessage.classList.add('hidden');
+        tooltip.hide();
+    }
+})
